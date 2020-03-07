bc43e1b81aaf373c931053bf181ef36840e10398
Axel Rammon <axelrammon48@gmail.com>
2020-03-07 11:01:35 -0300

    Mudando estrutura de pastas

---
+++ JS/Desafios/DesafiosM4/desafio03/desafio03.js
@@ -0,0 +1,35 @@
+var inputNome = document.querySelector('input');
+var btnBuscar = document.querySelector('button');
+var listaUl = document.querySelector('ul');
+
+btnBuscar.onclick = () => {
+
+  var userName = inputNome.value;
+
+  listaUl.innerHTML = ''
+
+  axios.get(`https://api.github.com/users/${userName}/repos`)
+  .then((response) => {
+    var repositorios = response.data;
+
+    for (repo of repositorios) {
+      var repoName = repo.name;
+      
+      var itemLista = document.createElement('li');
+      var repositorio = document.createTextNode(repoName);
+      
+      itemLista.appendChild(repositorio);
+      
+      listaUl.appendChild(itemLista);
+
+      inputNome.value = '';
+    }
+    
+    
+  }).catch((erro) => {
+    alert(`Usuário não existe.`);
+    inputNome.value = '';
+    console.log(erro);
+  });
+
+}
